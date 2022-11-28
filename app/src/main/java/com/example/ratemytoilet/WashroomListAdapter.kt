d8f0dafb16a557162045e8080571cddb80e6c64d
Wilson-Wu1 <wilsonwuuu@gmail.com>
2022-11-28 08:54:35 -0800

    Added Unknown value to amenities and hardcoded distance

--- app/src/main/java/com/example/ratemytoilet/WashroomListAdapter.kt
+++ app/src/main/java/com/example/ratemytoilet/WashroomListAdapter.kt
@@ -61,13 +61,13 @@ class WashroomListAdapter(private val context: Context, private var locationList
         var soapValue = ""
 
 
-        // Get average cleanliness rating
+        // Retrieve reviews for a location
         CoroutineScope(Dispatchers.IO).launch{
+
             var locationId = locationList[position].id
             val genderInt = locationList[position].gender
+
             var reviews = reviewViewModel.getReviewsForLocation(locationId)
-
-
             var arrayOfReviews = mutableListOf<Review>()
 
 
@@ -78,7 +78,7 @@ class WashroomListAdapter(private val context: Context, private var locationList
             }
 
 
-            // Make sure there exists a review, otherwise divide by 0 error
+            // Make sure there exists a review, otherwise divide by 0 error while calculating average cleanliness
             if(reviews.isNotEmpty()){
 
 
@@ -86,37 +86,66 @@ class WashroomListAdapter(private val context: Context, private var locationList
                 averageCleanlinessRating = (totalRatingAmount.toDouble() / reviews.size.toDouble())
 
 
+                // Sort reviews by timestamp in descending order
                 arrayOfReviews.sortWith(compareByDescending{it.dateAdded})
+
+
+                //TODO: Can I combine the two loops into one? Would it be faster?
+
+                // Loop through reviews, and find most recent review with sufficientPaperTowels set to either 0 or 1 (No or Yes)
+                var paperTowelIsUnknown = false
                 for(latestReview in arrayOfReviews){
-                    //println("Review: " + latestReview +" : " + latestReview.dateAdded)
                     if(latestReview.sufficientPaperTowels == 0){
                         paperTowelsValue = " / No Paper Towels"
+                        paperTowelIsUnknown = true
                         break
                     }
                     else if(latestReview.sufficientPaperTowels == 1){
                         paperTowelsValue = " / Paper Towels"
+                        paperTowelIsUnknown = true
                         break
                     }
                 }
 
+                // If no review contained sufficientPaperTowels set to either 0 or 1 (No or Yes), then set to Unknown status
+                if(!paperTowelIsUnknown){
+                    paperTowelsValue = " / Unknown"
+                }
+
+
+                // Loop through reviews, and find most recent review with sufficientSoap set to either 0 or 1 (No or Yes)
+                var soapIsUnknown = false
                 for(latestReview in arrayOfReviews){
-                    //println("Review: " + latestReview +" : " + latestReview.dateAdded)
                     if(latestReview.sufficientSoap == 0){
                         soapValue = " / No Soap"
+                        soapIsUnknown = true
                         break
                     }
                     else if(latestReview.sufficientSoap == 1){
                         soapValue = " / Soap"
+                        soapIsUnknown = true
                         break
                     }
                 }
 
+                // If no review contained sufficientSoap set to either 0 or 1 (No or Yes), then set to Unknown status
+                if(!soapIsUnknown){
+                    soapValue = " / Unknown"
+                }
 
+
+            }
+            else{
+                // If no reviews exist, then set amenity values to Unknown
+                paperTowelsValue = " / Unknown"
+                soapValue = " / Unknown"
             }
 
 
-            // Change the views
+            // Change the views. Cannot edit UI in coroutine
             GlobalScope.launch(Dispatchers.Main) {
+
+                // TODO: If rating is 0.0 (double), then set to Int (0). Is needed?
                 if(averageCleanlinessRating == 0.0){
                     ratingsText.text = averageCleanlinessRating.toInt().toString()
                 }
@@ -126,7 +155,7 @@ class WashroomListAdapter(private val context: Context, private var locationList
 
 
 
-                // Set gender
+                // Set gender and create amenities string
                 if(genderInt == 0){
                     amenities.text = "Male" + paperTowelsValue + soapValue
                 }
@@ -139,6 +168,11 @@ class WashroomListAdapter(private val context: Context, private var locationList
             }
 
         }
+
+
+        // TODO: Get user location, and find distance relative to washroom location
+        var distance = view.findViewById<TextView>(R.id.distance)
+        distance.text = "100m"
 
 
         // Set image for rating
