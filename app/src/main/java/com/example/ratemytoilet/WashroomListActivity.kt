34df669b57d80ae00650b47852fe851f59f7b1a0
kylewu.1999@gmail.com <kylewu.1999@gmail.com>
2022-12-03 14:35:58 -0800

    fix conflict in rebase

--- app/src/main/java/com/example/ratemytoilet/WashroomListActivity.kt
+++ app/src/main/java/com/example/ratemytoilet/WashroomListActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.ratemytoilet.database.Location
 import com.example.ratemytoilet.database.LocationViewModel
-import java.text.DateFormat
-import java.text.SimpleDateFormat
+import com.example.ratemytoilet.database.ReviewViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 /*
@@ -24,42 +27,49 @@ https://stackoverflow.com/questions/14666106/inserting-a-textview-in-the-middle-
 https://www.javatpoint.com/android-custom-listview
  */
 
-class WashroomListActivity : AppCompatActivity() {
+class WashroomListActivity : AppCompatActivity(), FilterDialogFragment.FilterListener {
 
     private lateinit var myListView: ListView
     private lateinit var arrayList: ArrayList<Location>
     private lateinit var arrayAdapter: WashroomListAdapter
     private val monthArray = arrayOf("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+    private lateinit var locationViewModel: LocationViewModel
+    private var maleCheck = false
+    private var femaleCheck = false
+    private var paperCheck = false
+    private var soapCheck = false
+    private var accessCheck = false
+    private var cleanlinessStart = 1f
+    private var cleanlinessEnd = 5f
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
+        var bundle = intent.extras
+        maleCheck = bundle?.getBoolean(MainActivity.MALE_CHECK_KEY) ?: false
+        femaleCheck = bundle?.getBoolean(MainActivity.FEMALE_CHECK_KEY) ?: false
+        paperCheck = bundle?.getBoolean(MainActivity.PAPER_CHECK_KEY) ?: false
+        soapCheck = bundle?.getBoolean(MainActivity.SOAP_CHECK_KEY) ?: false
+        accessCheck = bundle?.getBoolean(MainActivity.ACCESS_CHECK_KEY) ?: false
+        cleanlinessStart = bundle?.getFloat(MainActivity.CLEANLINESS_START_KEY) ?: 1f
+        cleanlinessEnd = bundle?.getFloat(MainActivity.CLEANLINESS_END_KEY) ?: 5f
 
         // Remove app name from toolbar
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
 
         // List of locations
-        myListView = findViewById<ListView>(R.id.lv_locations)
-
+        myListView = findViewById(R.id.lv_locations)
 
         // Arraylist for displaying entries
         arrayList = ArrayList<Location>()
         arrayAdapter = WashroomListAdapter(this, arrayList)
         myListView.adapter = arrayAdapter
-        var locationViewModel = LocationViewModel()
+        locationViewModel = LocationViewModel()
+        loadWashrooms()
 
-
-        // Reload the array with database values
-        locationViewModel.locations.observe(this, Observer { it ->
-            arrayAdapter.replace(it)
-            arrayAdapter.notifyDataSetChanged()
-        })
-
-
-        // TODO: When a location is clicked, show its reviews
         myListView.isClickable = true
         myListView.onItemClickListener = object : AdapterView.OnItemClickListener {
             override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
@@ -87,10 +97,75 @@ class WashroomListActivity : AppCompatActivity() {
                 viewIntent.putExtra("date", monthName + ". " + date.toString() + ", " +  year.toString())
                 viewIntent.putExtra("gender", gender)
                 startActivity(viewIntent)
+            }
+        }
+    }
 
+    private fun loadWashrooms() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            var newLocations = ArrayList<Location>()
+            var allLocations = locationViewModel.getAllLocations()
+            val reviewViewModel = ReviewViewModel()
+            if (allLocations != null &&
+                    (paperCheck ||
+                    soapCheck ||
+                    accessCheck ||
+                    maleCheck ||
+                    femaleCheck ||
+                    cleanlinessStart != 1f ||
+                    cleanlinessEnd != 5f)
+            ) {
+                for (location in allLocations) {
+                    var shouldAdd = true
+                    var rating = 0.0
+                    var allReviews = reviewViewModel.getReviewsForLocation(location.id)
+                    allReviews = allReviews.sortedByDescending { it.dateAdded }
+                    if (allReviews.isNotEmpty()) {
+                        for (review in allReviews) {
+                            rating += review.cleanliness
+                        }
+                        rating /= allReviews.size
 
+                        if (paperCheck) {
+                            if (allReviews[0].sufficientPaperTowels != 1) {
+                                shouldAdd = false
+                            }
+                        }
+                        if (soapCheck) {
+                            if (allReviews[0].sufficientSoap != 1) {
+                                shouldAdd = false
+                            }
+                        }
+                        if (accessCheck) {
+                            if (allReviews[0].accessibility != 1) {
+                                shouldAdd = false
+                            }
+                        }
+                    }
+                    if ((femaleCheck && !maleCheck) || (!femaleCheck && maleCheck)) {
+                        if (maleCheck) {
+                            if (location.gender != 0 && location.gender != 2) {
+                                shouldAdd = false
+                            }
+                        }
+                        if (femaleCheck) {
+                            if (location.gender != 1 && location.gender != 2) {
+                                shouldAdd = false
+                            }
+                        }
+                    }
 
+                    if (rating !in cleanlinessStart..cleanlinessEnd) {
+                        shouldAdd = false
+                    }
 
+                    if (shouldAdd) newLocations.add(location)
+                }
+            }
+
+            withContext(Main) {
+                arrayAdapter.replace(newLocations)
+                arrayAdapter.notifyDataSetChanged()
             }
         }
     }
@@ -107,12 +182,39 @@ class WashroomListActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.getItemId()) {
             R.id.action_maintain -> {
-                val filterDialog = FilterDialogFragment()
-                filterDialog.show(supportFragmentManager, "Filter")
-                // TODO: Return filter settings and filter reviews
+                val filterDialogFragment = FilterDialogFragment()
+                var bundle = Bundle()
+                bundle.putBoolean(MainActivity.MALE_CHECK_KEY, maleCheck)
+                bundle.putBoolean(MainActivity.FEMALE_CHECK_KEY, femaleCheck)
+                bundle.putBoolean(MainActivity.PAPER_CHECK_KEY, paperCheck)
+                bundle.putBoolean(MainActivity.SOAP_CHECK_KEY, soapCheck)
+                bundle.putBoolean(MainActivity.ACCESS_CHECK_KEY, accessCheck)
+                bundle.putFloat(MainActivity.CLEANLINESS_START_KEY, cleanlinessStart)
+                bundle.putFloat(MainActivity.CLEANLINESS_END_KEY, cleanlinessEnd)
+                filterDialogFragment.arguments = bundle
+                filterDialogFragment.show(supportFragmentManager, "Filter")
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onFilterConditionPassed(
+        paperCheck: Boolean,
+        soapCheck: Boolean,
+        accessCheck: Boolean,
+        maleCheck: Boolean,
+        femaleCheck: Boolean,
+        startValue: Float,
+        endValue: Float
+    ) {
+        this.paperCheck = paperCheck
+        this.soapCheck = soapCheck
+        this.accessCheck = accessCheck
+        this.maleCheck = maleCheck
+        this.femaleCheck = femaleCheck
+        this.cleanlinessStart = startValue
+        this.cleanlinessEnd = endValue
+        loadWashrooms()
     }
 }
